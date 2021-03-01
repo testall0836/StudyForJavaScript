2ce4487d7386568b66d608550b83e93633be978b
testall0836 <testall0836@gmail.com>
2021-03-01 21:07:45 +0900

    javascript Typing Game

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //사용 변수
-const GAME_TIME = 2;
+const GAME_TIME = 9;
 let score = 0;
 let time = GAME_TIME;
 let isPlaying = false;
@@ -20,36 +20,63 @@ const button = document.querySelector('.button');
 init();
 
 function init() {
+    buttonChange('게임 로딩 중');
     getWord();
     wordInput.addEventListener('input', checkMatch);
 }
 
 function checkStatus() {
     if (!isPlaying && time === 0) {
-        buttonChange("게임 종료");
+        buttonChange("게임시작");
         clearInterval(checkInterval);
     }
 }
 
 // 단어 불러오기
 function getWord() {
-    words = ['Hello', 'Banana', 'Apple', 'Cherry'];
-    buttonChange('게임시작');
+
+    axios.get('https://random-word-api.herokuapp.com/word?number=100')
+    .then(function (response) {
+      
+        response.data.forEach((word) => {
+            if(word.length < 10) {
+                words.push(word);
+            }
+        });
+        buttonChange('게임시작');
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
 }
 
+//단어 일치 검사
 function checkMatch () {
     if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+        wordInput.value = "";
+
+        if(!isPlaying) {
+            return;
+        }
+
         score++;
         scoreDisplay.innerText = score;
-        wordInput.value = "";
+        time = GAME_TIME;
+        const randomIndex = Math.floor(Math.random() * words.length);
+        wordDisplay.innerText = words[randomIndex];
     }
 }
 
 function run() {
     isPlaying = true;
     time = GAME_TIME;
+    wordInput.focus();
+
     timeInterval = setInterval(countDown, 1000);
     checkInterval = setInterval(checkStatus, 50);
+
+    buttonChange('게임중');
 }
 
 function countDown() {
