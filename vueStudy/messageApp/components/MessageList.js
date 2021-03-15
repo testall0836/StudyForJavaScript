5b5bbb9a2b0423ec504f9cce69c330e2e3c59164
testall0836 <testall0836@gmail.com>
2021-03-15 23:56:15 +0900

    study vue.js component2

--- vueStudy/messageApp/components/MessageList.js
+++ vueStudy/messageApp/components/MessageList.js
@@ -1,9 +1,13 @@
+import MessageListItem from './components/MessageListItem.js';
 export default {
     name: 'MessageList',
     template: `<ul>
     <li v-for="item in items" :item="item">
     {{ item.text }} - {{ item.createdAt }}
     <button @click="deleteMessage(item)">X</button></li></ul>`,
+    components: {
+        MessageListItem
+    },
     props: {
         items: {
             type: Array,
